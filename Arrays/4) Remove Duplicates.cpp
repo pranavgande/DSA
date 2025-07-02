bc7ae0fa4c6a08442dbff7d214e20384ee385254
Pranav Gande <186005448+pranavgande@users.noreply.github.com>
2025-07-02 22:52:43 +0530

    Update 4) Remove Duplicates.cpp

--- Arrays/4) Remove Duplicates.cpp
+++ Arrays/4) Remove Duplicates.cpp
@@ -34,6 +34,12 @@ int main() {
 
 
 //optimal-Two pointer approach
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int removeDuplicates(int arr[], int n)
+{
   int i = 0;
   for (int j = 1; j < n; j++) {
     if (arr[i] != arr[j]) {
@@ -43,3 +49,13 @@ int main() {
   }
   return i + 1;
 }
+int main() {
+  int arr[] = {1,1,2,2,2,3,3};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int k = removeDuplicates(arr, n);
+  cout << "The array after removing duplicate elements is " << endl;
+  for (int i = 0; i < k; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
