5e4ba6f7145509798c69a23febe7c9aa9da73fcf
Pranav Gande <186005448+pranavgande@users.noreply.github.com>
2025-07-04 22:22:37 +0530

    Update 5)  Left Rotate the Array be one.cpp

--- Arrays/5)  Left Rotate the Array be one.cpp
+++ Arrays/5)  Left Rotate the Array be one.cpp
@@ -1,2 +1,10 @@
 //brute force- store the first element at the lst and bring all other elements in the previous index
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotatearr(int arr[], int n)
+  int temp[n];
+  for (int i = 0; i<n;i++);
+    temp [i-1]= arr[i];
+temp[n-1]=arr[0];
