94b38139107fe880e49893b0af969d78849b6b1a
Pranav Gande <186005448+pranavgande@users.noreply.github.com>
2025-07-05 14:12:53 +0530

    Update 5)  Left Rotate the Array be one.cpp

--- Arrays/5)  Left Rotate the Array be one.cpp
+++ Arrays/5)  Left Rotate the Array be one.cpp
@@ -8,3 +8,11 @@ void rotatearr(int arr[], int n)
   for (int i = 0; i<n;i++);
     temp [i-1]= arr[i];
 temp[n-1]=arr[0];
+for(int i -0;i<n;i++)
+  cout << temp[i];
+
+int main(){
+  int arr[]={};
+  int n;
+  rotatearr(arr[], n);
+}
