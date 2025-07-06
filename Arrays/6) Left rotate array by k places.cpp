0d291084cbeb18e556aac691757f8f9f2486f479
Pranav Gande <186005448+pranavgande@users.noreply.github.com>
2025-07-06 09:43:34 +0530

    Update and rename 6) Left rotate array by d places.cpp to 6) Left rotate array by k places.cpp

---
+++ Arrays/6) Left rotate array by k places.cpp
@@ -0,0 +1,10 @@
+ /*Problem Statement: Given an array of integers, rotating array of elements by k elements either left or right.
+
+Examples:
+
+Example 1:
+Input: N = 7, array[] = {1,2,3,4,5,6,7} , k=2 , right
+Output: 6 7 1 2 3 4 5
+Explanation: array is rotated to right by 2 position .*/
+
+//Brute force- 
