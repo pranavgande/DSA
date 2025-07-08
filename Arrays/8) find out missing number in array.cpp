6b1679a9bd1549c48e8af93c8a8916e482c991e8
Pranav Gande <186005448+pranavgande@users.noreply.github.com>
2025-07-08 18:27:16 +0530

    Update 8) find out missing number in array.cpp
    brute force

--- Arrays/8) find out missing number in array.cpp
+++ Arrays/8) find out missing number in array.cpp
@@ -1,1 +1,28 @@
+/*Example 1:
+Input Format: N = 5, array[] = {1,2,4,5}
+Result: 3
+Explanation: In the given array, number 3 is missing. So, 3 is the answer.*/
 
+//Brute force- use the linear search
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int FindNumber(vector<int>&a, int N){
+  //outer loop runs from 1 to N
+  for(int i = 0; i<N; i++)
+    int flag = 0;
+  for(int j=0;j<N-1;j++)
+    if(arr[j]==i)
+      //if the element is found in array
+      int flag =1;
+      break;
+if(flag ==0) return i;
+
+
+int main(){
+  int N = ;
+  int arr[]={};
+  int answer = FindNumber(a,N);
+  return 0;
+}
