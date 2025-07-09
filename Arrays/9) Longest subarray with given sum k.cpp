8a150e924d37679646549d4ce8f17405b238b99f
Pranav Gande <186005448+pranavgande@users.noreply.github.com>
2025-07-09 20:33:07 +0530

    Update 9) Longest subarray with given sum k.cpp

--- Arrays/9) Longest subarray with given sum k.cpp
+++ Arrays/9) Longest subarray with given sum k.cpp
@@ -2,3 +2,24 @@
 Input Format: N = 3, k = 5, array[] = {2,3,5}
 Result: 2
 Explanation: The longest subarray with sum 5 is {2, 3}. And its length is 2.*/
+//brute force approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int LongestSubarr(vector<int>&a, long long k){
+  int n =  sizeof.arr(); //size of array
+  int len = 0;
+  for(int i = 0;i<n;i++)
+    for(int j = i;j<n;j++)
+      s+=a[k];
+  if(s==k)
+    len =  max(len, j-i+1);
+return len;
+}
+
+int main(){
+  vector<int> a ={};
+  long long k = //number;
+  int len = LongestSubarr(a,k);
+  return 0;
+}
